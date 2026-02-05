7a41ee33ffa052b03295daffc7d32814712d5c3d
himu0023 <himanshubisht3231@gmail.com>
2026-02-05 11:51:19 +0530

    moving with day 2

---
+++ DayTwo/ooooops.py
@@ -0,0 +1,13 @@
+class Country:
+    def __init__(self, name, continent):
+        self.name = name
+        self.continent = continent
+
+    def Green(self):
+        print(f"Hello welcome to {self.name}")
+
+cont = Country("India", "Asia")
+
+print(cont.name)
+print(cont.continent)
+cont.Green()
