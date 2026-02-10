8d8182ea2f4e3e44450b4bf9352caaf08c376079
himu0023 <himanshubisht3231@gmail.com>
2026-02-10 11:04:41 +0530

    understanding question no:167

---
+++ LeetCode/leetCode167.py
@@ -0,0 +1,17 @@
+def twoSum(nums, target):
+    num_map = {}  # value -> index
+    
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    
+    return []  # No solution
+
+number = [2,3,4,5,6]
+target = 7
+
+x = twoSum(number, target)
+
+print(x)
