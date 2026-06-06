266163fd729ad40a037ba749e5fdaf37dc4083c4
himu0023 <himanshubisht3231@gmail.com>
2026-06-06 20:14:24 +0530

    102

---
+++ LeetCode/leetCode102.py
@@ -0,0 +1,49 @@
+from collections import deque
+from typing import Optional 
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode])-> list[list[int]]:
+        if not root: 
+            return []
+        
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level_size = len(queue)
+            level = []
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+
+        return result
+
+# Example usage:
+# Constructing the binary tree:
+#       3
+#      / \
+#     9   20
+#        /  \
+#       15   7
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+solution = Solution()
+print(solution.levelOrder(root))  # Output: [[3], [9, 20], [15, 7]]
